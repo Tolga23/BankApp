010ef8fe10b9dc9b98bf7a15c79748f1fc17cbdf
Tolga <10477308+Tolga23@users.noreply.github.com>
2023-08-02 11:44:37 +0300

    add logger with kafka

--- src/main/java/com/thardal/bankapp/global/exception/GlobalCustomExceptionHandler.java
+++ src/main/java/com/thardal/bankapp/global/exception/GlobalCustomExceptionHandler.java
@@ -3,6 +3,9 @@ package com.thardal.bankapp.global.exception;
 import com.thardal.bankapp.global.dto.RestResponse;
 import com.thardal.bankapp.global.exceptions.BusinessException;
 import com.thardal.bankapp.global.exceptions.ItemNotFoundException;
+import com.thardal.bankapp.kafka.dto.KafkaMessage;
+import com.thardal.bankapp.log.service.LogService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,9 +13,59 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Date;
+
 @RestController
 @ControllerAdvice
+@RequiredArgsConstructor
 public class GlobalCustomExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private final LogService logService;
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleException(Exception ex) {
+        String errorMessage = "An unexpected error occurred: " + ex.getMessage();
+        RestResponse<String> restResponse = RestResponse.error(errorMessage);
+        restResponse.setMessage(errorMessage);
+
+        logError(errorMessage);
+
+        return new ResponseEntity<>(restResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<Object> handleBusinessException(BusinessException ex) {
+        String errorMessage = "An business error occurred: " + ex.getBaseErrorMessages().getMessage();
+        RestResponse<String> restResponse = RestResponse.error(errorMessage);
+        restResponse.setMessage(errorMessage);
+
+        logError(errorMessage);
+
+        return new ResponseEntity<>(restResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ItemNotFoundException.class)
+    public ResponseEntity<Object> handleCustomException(ItemNotFoundException ex) {
+        String errorMessage = "An error occurred: " + ex.getBaseErrorMessages().getMessage();
+
+        RestResponse<String> restResponse = RestResponse.error(errorMessage);
+        restResponse.setMessage(errorMessage);
+
+        logError(errorMessage);
+
+        return new ResponseEntity<>(restResponse, HttpStatus.NOT_FOUND);
+    }
+
+    private void logError(String errorMessage) {
+        KafkaMessage logMessage = KafkaMessage.builder()
+                .message(errorMessage)
+                .dateTime(new Date())
+                .build();
+
+        logService.log(logMessage);
+    }
+}
+
 
 //    @ExceptionHandler
 //    public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest webRequest) {
@@ -24,31 +77,3 @@ public class GlobalCustomExceptionHandler extends ResponseEntityExceptionHandler
 //
 //        return new ResponseEntity<>(globalExceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
 //    }
-
-    @ExceptionHandler(ItemNotFoundException.class)
-    public ResponseEntity<Object> handleCustomException(ItemNotFoundException ex) {
-        String errorMessage = "An error occurred: " + ex.getBaseErrorMessages().getMessage();
-
-        RestResponse<String> restResponse = RestResponse.error(errorMessage);
-        restResponse.setMessage(errorMessage);
-
-        return new ResponseEntity<>(restResponse, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<Object> handleException(Exception ex) {
-        String errorMessage = "An unexpected error occurred: " + ex.getMessage();
-        RestResponse<String> restResponse = RestResponse.error(errorMessage);
-        restResponse.setMessage(errorMessage);
-
-        return new ResponseEntity<>(restResponse, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
-    @ExceptionHandler(BusinessException.class)
-    public ResponseEntity<Object> handleBusinessException(BusinessException ex) {
-        String errorMessage = "An business error occurred: " + ex.getBaseErrorMessages().getMessage();
-        RestResponse<String> restResponse = RestResponse.error(errorMessage);
-        restResponse.setMessage(errorMessage);
-        return new ResponseEntity<>(restResponse, HttpStatus.NOT_FOUND);
-    }
-}
